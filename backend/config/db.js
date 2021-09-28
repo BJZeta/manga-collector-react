83712fc11ddc8acbb8e4ad0fce4bd85d845394ca
Brandon Unzueta <unzueta.brandon@gmail.com>
2021-09-27 19:50:19 -0700

    updating mongodb connection

--- backend/config/db.js
+++ backend/config/db.js
@@ -9,7 +9,7 @@ const connectDB = async () => {
     });
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (err) {
-    console.error(`Error: ${err.message}`);
+    console.error(`Error in connection: ${err.message}`);
     process.exit(1);
   }
 };
